89cdb1f9d4cd857a8552ed9db5c83274eb24cf7e
magicishaq <ishaqkhan1993@gmail.com>
2019-11-07 11:00:04 +0000

    Added cart service. And purchase button

---
+++ src/app/cart.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root' 
+})
+export class CartService {
+
+  items = []; 
+  addToCart(product){
+    this.items.push(product); 
+  }
+  getItems(product){
+    return this.items; 
+  }
+
+  clearCart(product){
+    this.items = []; 
+    return this.items; 
+  }
+
+}
