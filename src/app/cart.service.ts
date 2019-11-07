d5b86e9cfcd2090086e6ffbfc680b47d9e89a185
magicishaq <ishaqkhan1993@gmail.com>
2019-11-07 13:13:49 +0000

    added shipping component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'; 
 @Injectable({
   providedIn: 'root' 
 })
 export class CartService {
 
   items = []; 
+  constructor(private http: HttpClient){
+
+  }
   addToCart(product){
     this.items.push(product); 
   }
@@ -16,6 +19,10 @@ export class CartService {
   clearCart(product){
     this.items = []; 
     return this.items; 
+  }
+
+  getShippingPrices(product){
+    return this.http.get('/assets/shipping.json'); 
   }
 
 }
